5b40e1590f4ee6c2185ea6661a3d65e414955125
Komrad71 <an.vasichkin@gmail>
2025-01-12 18:41:00 +0200

    finaly work

--- src/App.jsx
+++ src/App.jsx
@@ -1,27 +1,70 @@
-import Profile from './components/profile/Profile';
-import userData from './components/profile/userData.json';
-
-import FriendList from './components/friendList/FriendList';
-import friends from './components/friendList/friends.json';
-
-import TransactionHistory from './components/transactionHistory/TransactionHistory';
-import transactions from './components/transactionHistory/transactions.json';
-
+import { useState,  useEffect } from 'react'
+import Description from './components/deskription/Description.jsx';
+import Feedback from './components/feedback/Feedback.jsx';
+import  Options from './components/options/Options.jsx'
+import  Notification from './components/notification/Notification.jsx'
 
 const App = () => {
-  return (
-      <>
-          <Profile
-              userName={userData.username}
-              tag={userData.tag}
-              location={userData.location}
-              image={userData.avatar}
-              stats={userData.stats}
-          />
-          <FriendList friends={friends} />
-          <TransactionHistory items={transactions} />
-      </>
-  );
+	const [feedback, setFeedback] = useState(() => {
+		const savedFeedback = window.localStorage.getItem("feedback");
+
+		if (savedFeedback) {
+			return JSON.parse(savedFeedback);
+		}
+
+		return {
+			good: 0,
+			neutral: 0,
+			bad: 0,
+		};
+	});
+
+	useEffect(() => {
+		window.localStorage.setItem("feedback", JSON.stringify(feedback));
+	}, [feedback]);
+
+
+	
+	const total = feedback.good + feedback.neutral + feedback.bad;
+	const positivePercentage = Math.round((feedback.good / total) * 100);
+
+	const updateFeedback = (feedbackType) => {
+		if (feedbackType === "reset") {
+			setFeedback({
+				good: 0,
+				neutral: 0,
+				bad: 0,
+			});
+			return;
+		}
+		return setFeedback({
+			...feedback,
+			[feedbackType]: feedback[feedbackType] + 1,
+		});
+	};
+
+	return (
+		<main>
+			<section>
+				<Description />
+				<Options
+					onUpdate={updateFeedback}
+					totalFeedback={total}
+				/>
+				{total ? (
+					<Feedback
+						good={feedback.good}
+						neutral={feedback.neutral}
+						bad={feedback.bad}
+						total={total}
+						positivePercentage={positivePercentage}
+					/>
+				) : (
+					<Notification message="No feedback yet!" />
+				)}
+			</section>
+		</main>
+	);
 };
 
 export default App;
